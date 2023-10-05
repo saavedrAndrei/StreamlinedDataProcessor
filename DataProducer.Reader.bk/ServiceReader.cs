ef4032ca19c99f4f2e2f6a93cd8d3bb502423d28
Andrei Saavedra <asaavedra@nalcom.com>
2023-10-05 21:20:02 +0200

    Microservice Structure 2.0

---
+++ DataProducer.Reader.bk/ServiceReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace DataProducer.Reader
+{
+    public class ServiceReader 
+    {
+        public async Task ListenerFileAsync(string filePath)
+        {
+            // Implement your file processing logic here
+            // You can use async methods for file processing operations
+
+            await Task.Delay(TimeSpan.FromSeconds(5)); // Simulating some asynchronous processing
+            Console.WriteLine($"Processed file: {filePath}");
+        }
+    }
+}
---
+++ DataProducer.Reader/FileReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataProducer.Reader
+{
+    public delegate void NewFileDetectedEventHandler(object sender, string filePath);
+
+    public class FileReader : IDisposable
+    {
+        private string folderPath;
+        private FileSystemWatcher fileSystemWatcher;
+        private List<string> processedFiles = new List<string>();
+
+        // Event to notify when a new file is detected
+        public event NewFileDetectedEventHandler NewFileDetected;
+
+        public FileReader(string folderPath)
+        {
+            this.folderPath = folderPath;
+
+            if (!Directory.Exists(folderPath))
+            {
+                throw new DirectoryNotFoundException($"Folder not found: {folderPath}");
+            }
+
+            // Create a FileSystemWatcher to monitor the folder
+            fileSystemWatcher = new FileSystemWatcher(folderPath);
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string newFullPath = e.FullPath;
+
+            if (!IsFileProcessed(newFullPath))
+            {
+                // Process the newly created file here
+                Console.WriteLine($"New file detected: {e.FullPath}");
+                processedFiles.Add(newFullPath);
+
+                // Raise the event to notify the main program
+                NewFileDetected?.Invoke(this, newFullPath);
+            }
+        }
+
+        private bool IsFileProcessed(string path)
+        {
+            return processedFiles.Contains(path);
+        }
+
+        public void Dispose()
+        {
+            fileSystemWatcher.Dispose();
+        }
+
+    }
+}
